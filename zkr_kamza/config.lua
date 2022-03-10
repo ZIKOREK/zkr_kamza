fd0dd55af13fcad8fd29451e09abf3837ada87c8
ZikOR <80882152+ZIKOREK@users.noreply.github.com>
2022-03-10 22:08:46 +0100

    Update config.lua

--- zkr_kamza/config.lua
+++ zkr_kamza/config.lua
@@ -1,9 +1,9 @@
 --Aby dodać link do webhooka wejdź w server.lua i podmień local webhook = '' na local webhook 'https://twojwebhook' (5 linijka kodu)
 Config = {}
 
-Config.NazwaBota = 'zkr_kamza' -- Write the desired bot name
+Config.NazwaBota = 'zkr_kamza' -- Nazwa Bota 
 
-Config.NazwaSerwera = 'zkr_kamza' -- Write your server's name
+Config.NazwaSerwera = 'zkr_kamza' -- Nazwa Serwera
 
 Config.AvatarURL = 'https://cdn.discordapp.com/attachments/942824742593912902/943561558385111060/Bez_nazwy-dsadasasads1.png' -- Wklej zdjęcie avataru bota
 
